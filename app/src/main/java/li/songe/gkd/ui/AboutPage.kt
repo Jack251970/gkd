da9e29a5de6ed94adb2833d4870b3593d7723dac
Jack251970 <1160210343@qq.com>
2023-11-18 23:43:26 +0800

    Put author names into array.xml.

--- app/src/main/java/li/songe/gkd/ui/AboutPage.kt
+++ app/src/main/java/li/songe/gkd/ui/AboutPage.kt
@@ -6,11 +6,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -65,7 +64,7 @@ fun AboutPage() {
             Text(text = stringResource(R.string.app_desc_appendix),
                 color = MaterialTheme.colorScheme.secondary,
                 style = MaterialTheme.typography.bodySmall)
-            Spacer(modifier = Modifier.height(10.dp))
+            Divider()
             Text(text = stringResource(R.string.version_code) + BuildConfig.VERSION_CODE,
                 style = MaterialTheme.typography.bodyMedium)
             Text(text = stringResource(R.string.version_name) + BuildConfig.VERSION_NAME,
@@ -75,54 +74,29 @@ fun AboutPage() {
             Text(text = stringResource(R.string.build_type) + BuildConfig.BUILD_TYPE,
                 style = MaterialTheme.typography.bodyMedium)
             Row {
+                val authors = context.resources.getStringArray(R.array.authors)
                 Text(text = stringResource(R.string.project_author),
                     style = MaterialTheme.typography.bodyMedium)
-                Text(text = "lisonge",
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = MaterialTheme.colorScheme.primary,
-                    modifier = Modifier.clickable {
-                        appScope.launchTry {
-                            // ActivityNotFoundException
-                            // https://bugly.qq.com/v2/crash-reporting/crashes/d0ce46b353/117002?pid=1
-                            context.startActivity(
-                                Intent(
-                                    Intent.ACTION_VIEW, Uri.parse("https://github.com/lisonge")
+                authors.forEach {
+                    Text(text = it,
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier.clickable {
+                            appScope.launchTry {
+                                // ActivityNotFoundException
+                                // https://bugly.qq.com/v2/crash-reporting/crashes/d0ce46b353/117002?pid=1
+                                context.startActivity(
+                                    Intent(
+                                        Intent.ACTION_VIEW, Uri.parse("https://github.com/$it")
+                                    )
                                 )
-                            )
-                        }
-                    })
-                Text(text = stringResource(R.string.and_divider),
-                    style = MaterialTheme.typography.bodyMedium)
-                Text(text = "lliioollcn",
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = MaterialTheme.colorScheme.primary,
-                    modifier = Modifier.clickable {
-                        appScope.launchTry {
-                            // ActivityNotFoundException
-                            // https://bugly.qq.com/v2/crash-reporting/crashes/d0ce46b353/117002?pid=1
-                            context.startActivity(
-                                Intent(
-                                    Intent.ACTION_VIEW, Uri.parse("https://github.com/lliioollcn")
-                                )
-                            )
-                        }
-                    })
-                Text(text = stringResource(R.string.and_divider),
-                    style = MaterialTheme.typography.bodyMedium)
-                Text(text = "Jack251970",
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = MaterialTheme.colorScheme.primary,
-                    modifier = Modifier.clickable {
-                        appScope.launchTry {
-                            // ActivityNotFoundException
-                            // https://bugly.qq.com/v2/crash-reporting/crashes/d0ce46b353/117002?pid=1
-                            context.startActivity(
-                                Intent(
-                                    Intent.ACTION_VIEW, Uri.parse("https://github.com/Jack251970")
-                                )
-                            )
-                        }
-                    })
+                            }
+                        })
+                    if (it != authors.last()) {
+                        Text(text = stringResource(R.string.and_divider),
+                            style = MaterialTheme.typography.bodyMedium)
+                    }
+                }
             }
             Row {
                 Text(text = stringResource(R.string.opensource_address),
