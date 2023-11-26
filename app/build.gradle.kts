c438002700b81860be97ded882eb0d6ea87db361
Jack251970 <1160210343@qq.com>
2023-11-26 14:06:42 +0800

    update: 新增英文支持

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
 
         applicationId = "li.songe.gkd"
         versionCode = 14
-        versionName = "1.5.4.2"
+        versionName = "1.5.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
